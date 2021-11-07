6166f2b9f20c631a451711723a11dc7687e365b4
re4rk <jois0157@gmail.com>
2021-11-08 01:18:26 +0900

    etc

--- src/BigNumber.c
+++ src/BigNumber.c
@@ -84,6 +84,62 @@ void Write_MPZ_File(SINT8 *filename, MPZ *MPZs,SINT32 bytes, SINT32 num){
   fclose(pFile);
 }
 
+void BIG_PRINT(MPZ *MPZs){
+  for(int j=MPZs->len-1; 0<=j ; j--)
+    printf("%0.8x",MPZs->dat[j]);
+  printf(" [%d][%d]\n",MPZs->len,MPZs->sign);
+}
+
+void COPY_MPZ(MPZ *r,MPZ *a){
+  for(int i=0;i<a->len;i++)
+    r->dat[i] = a->dat[i];
+  r->len = a->len;
+  r->sign = a->sign;
+}
+
+int Compare_MPZ(MPZ *first, MPZ *second){
+  if(first->sign == 0 && second->sign == 1) return 1;
+  else if(first->sign == 1 && second->sign == 0) return 0;
+  else if(first->sign == 0 && second->sign == 0){
+    if(first->len > second->len) return 1;
+    else if(second->len < first->len)return 0;
+
+    for(int i=first->len-1 ; 0 <= i;i--){
+      if(first->dat[i] > second->dat[i]) return 1;
+      else if(first->dat[i] < second->dat[i]) return 0;
+    }
+  }
+  else if(first->sign == 1 && second->sign == 0){
+    if(first->len > second->len) return 0;
+    else if(second->len < first->len) return 1;
+
+    for(int i=first->len-1 ; 0 <= i;i--){
+      if(first->dat[i] > second->dat[i]) return 0;
+      else if(first->dat[i] < second->dat[i]) return 1;
+    }
+  }
+  return 1;
+}
+
+int MPZ_Swap(MPZ *first, MPZ *second){
+  first->sign ^= second->sign ^=first->sign ^= second->sign;
+  first->len ^= second->len ^= first->len ^= second->len;
+
+  UINT32 *temp;
+  temp = first->dat;
+  first->dat = second->dat;
+  second->dat =temp;
+  return 0;
+}
+
+int MPZ_Sort(MPZ *MPZs,int size){
+  for(int i=0;i<size-1;i++)
+    for(int j=i+1;j<size;j++)
+      if(Compare_MPZ(&MPZs[i],&MPZs[j]) == 1)
+        MPZ_Swap(&MPZs[i],&MPZs[j]);
+  return 0;
+}
+
 int MPZ_UADD(MPZ *first, MPZ *second, MPZ *result){
   int overflow = 0;
   UINT32 temp;
@@ -206,49 +262,6 @@ int MPZ_SUB(MPZ *first, MPZ *second, MPZ *result){
   return 0;
 }
 
-int Compare_MPZ(MPZ *first, MPZ *second){
-  if(first->sign == 0 && second->sign == 1) return 1;
-  else if(first->sign == 1 && second->sign == 0) return 0;
-  else if(first->sign == 0 && second->sign == 0){
-    if(first->len > second->len) return 1;
-    else if(second->len < first->len)return 0;
-
-    for(int i=first->len-1 ; 0 <= i;i--){
-      if(first->dat[i] > second->dat[i]) return 1;
-      else if(first->dat[i] < second->dat[i]) return 0;
-    }
-  }
-  else if(first->sign == 1 && second->sign == 0){
-    if(first->len > second->len) return 0;
-    else if(second->len < first->len) return 1;
-
-    for(int i=first->len-1 ; 0 <= i;i--){
-      if(first->dat[i] > second->dat[i]) return 0;
-      else if(first->dat[i] < second->dat[i]) return 1;
-    }
-  }
-  return 1;
-}
-
-int MPZ_Swap(MPZ *first, MPZ *second){
-  first->sign ^= second->sign ^=first->sign ^= second->sign;
-  first->len ^= second->len ^= first->len ^= second->len;
-
-  UINT32 *temp;
-  temp = first->dat;
-  first->dat = second->dat;
-  second->dat =temp;
-  return 0;
-}
-
-int MPZ_Sort(MPZ *MPZs,int size){
-  for(int i=0;i<size-1;i++)
-    for(int j=i+1;j<size;j++)
-      if(Compare_MPZ(&MPZs[i],&MPZs[j]) == 1)
-        MPZ_Swap(&MPZs[i],&MPZs[j]);
-  return 0;
-}
-
 inline void MPZ_MUL_BLOCK(UINT32 first,UINT32 second,UINT32 *result){
   UINT64 temp = (UINT64)first * (UINT64)second;
   if((result[0]+=(UINT32)temp) < (UINT32)temp)
@@ -288,39 +301,25 @@ void MPZ_MUL_with_single_block(UINT32 first, MPZ *second,MPZ *result){
   return;
 }
 
-void COPY_MPZ(MPZ *r,MPZ *a){
-  for(int i=0;i<a->len;i++)
-    r->dat[i] = a->dat[i];
-  r->len = a->len;
-  r->sign = a->sign;
-}
-
 void MPZ_BIT_SHIFT(MPZ *r, MPZ *a, SINT32 shift){
-  int i=0;
   if(shift == 0){
     COPY_MPZ(r,a);
   }
-	else if (shift > 0)
-	{
+	else if (shift > 0){
 		r->sign = a->sign;
 		r->len = a->len + 1;
 		r->dat[a->len] = a->dat[a->len - 1] >> (32 - shift);
-		for (i = a->len - 1; i >= 1; i--)
-		{
+		for (int i = a->len - 1; i >= 1; i--)
 			r->dat[i] = (a->dat[i] << shift) | (a->dat[i - 1] >> (32 - shift));
-		}
 		r->dat[0] = a->dat[0] << shift;
 		if (r->dat[a->len] == 0)
 			r->len = a->len;
 	}
-	else
-	{
+	else{
 		r->sign = a->sign;
 		r->len = a->len;
-		for (i = 0; i < a->len-1; i++)
-		{
+		for (int i = 0; i < a->len-1; i++)
 			r->dat[i] = (a->dat[i] >> (-shift)) | (a->dat[i + 1] << (32 + shift));
-		}
 		r->dat[a->len - 1] = a->dat[a->len - 1] >> (-shift);
 		if (r->dat[a->len - 1] == 0)
 			r->len = a->len - 1;
@@ -343,23 +342,17 @@ void MPZ_WORD_SHIFT(MPZ* r, MPZ* a, SINT32 shift){
   }
 }
 
-void BIG_PRINT(MPZ *MPZs){
-  for(int j=MPZs->len-1; 0<=j ; j--)
-    printf("%0.8x",MPZs->dat[j]);
-  printf(" [%d][%d]\n",MPZs->len,MPZs->sign);
-}
-
 // normalization
 void MPZ_UDIV(MPZ *q, MPZ *r,MPZ *a, MPZ *b){
 	SINT32 i, normbits=0;
-	UINT32 x0, x1, x2;
+  UINT32 xx[3];
 	UINT64 x_, y_;
   MPZ *x = newMPZ(DATA_SIZE); 
   MPZ *y = newMPZ(DATA_SIZE);
   MPZ *tmp = newMPZ(DATA_SIZE);
   MPZ *tmp2 = newMPZ(DATA_SIZE);
 
-	if (0){
+	if (Compare_MPZ(a, b) == 0){
 		q->len = 0;
 		q->sign = 0;
 		COPY_MPZ(r, a);
@@ -372,12 +365,11 @@ void MPZ_UDIV(MPZ *q, MPZ *r,MPZ *a, MPZ *b){
 
 		MPZ_BIT_SHIFT(x, a, normbits);
 		MPZ_BIT_SHIFT(y, b, normbits);
-    /// init q
+    /// round 1
 		q->len = x->len - y->len+1;
 		for (i = 0; i < q->len; i++)
 			q->dat[i] = 0;
-
-
+    // round 2
     MPZ_WORD_SHIFT(tmp, y, x->len - y->len);
 		if (Compare_MPZ(x, tmp) == 1){
 			q->dat[x->len - y->len]++;
@@ -389,25 +381,23 @@ void MPZ_UDIV(MPZ *q, MPZ *r,MPZ *a, MPZ *b){
 			if (x->dat[i] == y->dat[y->len - 1])
 				q->dat[i - y->len] = 0xFFFFFFFF;
 			else
-				q->dat[i - y->len] = (((UINT64)x->dat[i] << 32) | x->dat[i - 1]) / y->dat[y->len - 1];
-
+				q->dat[i - y->len] = (((UINT64) x->dat[i] << 32) | x->dat[i - 1] ) / y->dat[y->len - 1];
       /// round 3.2
-			x_=(UINT64)q->dat[i - y->len] * y->dat[y->len - 2];
-			x0=x_ & 0xffffffff;
-			y_ = (UINT64)q->dat[i - y->len] * y->dat[y->len - 1];
-			y_ += x_ >> 32;
-			x1=y_ & 0xffffffff;
-			x2 = y_ >> 32;
-			if (x2 > x->dat[i])
-				q->dat[i - y->len]--;
-			else if (x2 == x->dat[i])
-			{
-				if(x1>x->dat[i-1])
-					q->dat[i - y->len]--;
-				else if(x1 == x->dat[i-1])
-					if(x0>x->dat[i-2])
-						q->dat[i - y->len]--;
-			}
+
+			x_ = (UINT64) q->dat[i - y->len] * y->dat[y->len - 2];
+			y_ = (UINT64) q->dat[i - y->len] * y->dat[y->len - 1] + (x_ >> 32);
+
+			xx[0] = x_ & 0xffffffff;
+			xx[1] = y_ & 0xffffffff;
+			xx[2] = y_ >> 32;
+
+      for(int m=2; 0<=m; m--){
+        if(xx[m] > x->dat[i+m-2]){
+          q->dat[i - y->len]--;
+          break;
+        }else if(xx[m] < x->dat[i+m-2])
+          break;
+      }
       /// round 3.3
       MPZ_WORD_SHIFT(tmp, y, x->len - y->len - 1);
       MPZ_MUL_with_single_block(q->dat[i - y->len], tmp,tmp2);
@@ -421,6 +411,7 @@ void MPZ_UDIV(MPZ *q, MPZ *r,MPZ *a, MPZ *b){
       }
 		}
 	}
+  // round 4
   MPZ_BIT_SHIFT(r, x, -normbits);
   for(i=q->len-1;0<=i;i--){
     if(q->dat[i] != 0)
