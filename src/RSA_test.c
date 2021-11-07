434f3a27e961b415cb401a6763ea758c20e08eb9
re4rk <jois0157@gmail.com>
2021-11-08 02:51:01 +0900

    src fix

--- src/RSA_test.c
+++ src/RSA_test.c
@@ -96,18 +96,21 @@ void test_DIV(){
     Read_MPZ_FIle("/Users/Re4rk/Documents/crypto/RSA/tmp/div_a.txt",diva, num);
     Read_MPZ_FIle("/Users/Re4rk/Documents/crypto/RSA/tmp/div_b.txt",divb, num);
     Read_MPZ_FIle("/Users/Re4rk/Documents/crypto/RSA/tmp/div_m.txt",divc, num);
+    
+    for(int i=0;i<num;i++)
+        MPZ_MUL(&diva[i],&divb[i],&result[i]);
 
     clock_t start = clock();
-    for(int i=0;i<num;i++){
-        MPZ_MUL(&diva[i],&divb[i],&result[i]);
+    
+    for(int i=0;i<num;i++)
         MPZ_UDIV(&q[i],&r[i],&result[i],&divc[i]);
-    }
+
+    clock_t end = clock();
+    printf("Time: %lf\n", (double)(end - start)/CLOCKS_PER_SEC);
 
     Write_MPZ_File("/Users/Re4rk/Documents/crypto/RSA/tmp/div_q_before.txt",result,bytes*2,num);
     Write_MPZ_File("/Users/Re4rk/Documents/crypto/RSA/tmp/div_q.txt",q,bytes,num);
     Write_MPZ_File("/Users/Re4rk/Documents/crypto/RSA/tmp/div_r.txt",r,bytes,num);
-    clock_t end = clock();
-    printf("Time: %lf\n", (double)(end - start)/CLOCKS_PER_SEC);
 
     delMPZs(diva, num);
     delMPZs(divb, num);
